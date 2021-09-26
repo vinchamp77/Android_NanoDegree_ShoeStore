e20d3bc6e0263db4f2665c93d1dc2c1e74de29d3
vinchamp77 <vinchamp77@gmail.com>
2021-09-26 22:57:48 +1000

    Add Login Fragment Layout and initial navigation

---
+++ app/src/main/java/com/udacity/shoestore/LoginFragment.kt
@@ -0,0 +1,22 @@
+package com.udacity.shoestore
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.udacity.shoestore.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        val binding = FragmentLoginBinding.inflate(inflater)
+
+        return binding.root
+    }
+}
